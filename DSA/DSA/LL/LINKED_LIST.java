942eb11324a9f40b9105c638a1f9062b76696f90
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2024-11-26 21:04:43 +0530

    Add files via upload

--- DSA/DSA/LL/LINKED_LIST.java
+++ DSA/DSA/LL/LINKED_LIST.java
@@ -16,6 +16,7 @@ class Node{
 	
 }
 public class LINKED_LIST {
+	private static Node head;
 	public static Node convertArr2LL(int[] ar) {
 		Node head = new Node(ar[0]);
 		Node mover = head;
@@ -55,8 +56,56 @@ public class LINKED_LIST {
 		}
 		return -1;
 	}
+	
+	public static Node insertHead(Node head,int val) {
+		Node temp = new Node(val,head);
+		return temp;
+	}
+	
+	public static Node insertTail(Node head,int val) {
+		if(head == null) {
+			Node temp = new Node(val);
+			return temp;
+		}
+		Node temp = head;
+		while(head.next!=null) {
+			temp = temp.next;
+		}
+		Node newNode = new Node(val);
+		temp.next = newNode;
+		return newNode;
+	}
+	
+	public static Node insertPostition(Node head,int val,int k) {
+		if(head == null) {
+			if(k==1) {
+				Node temp = new Node(val);
+			}else {
+				return head;
+			}
+		}
+		if(k==1) {
+			Node temp = new Node(val,head);
+			return temp;
+		}
+		int cnt = 1;
+		Node temp = head;
+		while(temp!=null) {
+			cnt++;
+			if(cnt == k-1) {
+				Node x = new Node(val,temp.next);
+//				x.next = temp.next;
+				temp.next = x;
+				break;
+			}
+			temp = temp.next;
+		}
+		return head;
+	}
 	public static void main(String[] args) {
 		int[] ar = {2,5,6,8};
+/*
+ * 		
 //		Node y = new Node(ar[0]);
 //		System.out.println(y.data);
 		
@@ -68,7 +117,15 @@ public class LINKED_LIST {
 		System.out.println(lenght);
 		
 		int res = checkPresent(head, 5);
-		System.out.println(res);
+//		System.out.println(res);
+		
+		head = insertHead(head, 100);
+//		System.out.println(head.data);
+		Traversal(head);
+ */
+		Node head = convertArr2LL(ar);
+		insertPostition(head, 100, 4);
+		Traversal(head);
 	}
 
 }
